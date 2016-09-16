dbbee6fce6c6619878a1e5b69418ae4b6f198986
Matthias Cullmann <matthias.cullmann@gmail.com>
2016-09-16 15:08:31 +0200

    3 fragen

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var fadeDuration = 200;
+function chain(from, to){
+	$(from).change(function() {
+		var val = $(from+" option:selected").text();
+		if (val == '...') {
+			$(to).fadeOut( fadeDuration );
+		} else {
+			$(to).fadeIn( fadeDuration );
+		}
+	});
+}
+
+chain("#i_wer", "#g_wasGrob");
+chain("#i_wasGrob", "#g_wasGenau");
+
