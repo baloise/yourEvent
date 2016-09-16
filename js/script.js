68abede01b2c360c10a7b4d5293b656960668adf
Matthias Cullmann <matthias.cullmann@gmail.com>
2016-09-16 18:21:12 +0200

    dauer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+document.eventForm.reset()
 var fadeDuration = 200;
 function chain(from, to){
 	$(from).change(function() {
@@ -14,12 +15,31 @@ chain("#i_wer", "#g_wasGrob");
 chain("#i_wasGrob", "#g_wasGenau");
 chain("#i_wasGenau", "#g_wann");
 
+$("#i_dauer").change(function() {
+	var val = $("#i_dauer option:selected").text();
+	if (val != 'bis zum') {
+		$("#g_bis").fadeOut( fadeDuration );
+	} else {
+		$("#g_bis").fadeIn( fadeDuration );
+	}
+});
 
 var i_wann = $('#i_wann').datetimepicker(
 		{
             format: 'MMMM Do YYYY'
         }		
 );
+
+$("#i_wann").on("dp.change", function(e) {
+	$("#g_dauer").fadeIn( fadeDuration );
+});
+
+var i_bis = $('#i_bis').datetimepicker(
+		{
+            format: 'MMMM Do YYYY'
+        }		
+);
+
 $('#eventForm').validator(
 {
 	custom: {
